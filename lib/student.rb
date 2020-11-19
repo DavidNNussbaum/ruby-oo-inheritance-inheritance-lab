2083c8d3528e31671a15a9738bfbc262cceff4b7
DavidNNussbaum <brocknynj@aol.com>
2020-11-18 20:55:09 -0500

    commit tree 86bf7dc966a66ef2d38b8d20698d59113c38a2d9

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+# require_relative "./user.rb"
+class Student < User
+# attr_accessor :first, :last
+def initialize
+    @knowledge = []
+end
+
+def learn(str)
+    @knowledge << str
+end
+
+# def first_name
+#     @first
+# end
+
+def knowledge
+    @knowledge
+end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,15 @@
+class User
+    attr_accessor :first_name, :last_name
+    # def initialize(first, last)
+    #     @first = first
+    #     @last = last
+    # end
+
+    # def first_name
+    #     @first
+    # end
+
+    # def last_name
+    #     @last
+    # end
+end
